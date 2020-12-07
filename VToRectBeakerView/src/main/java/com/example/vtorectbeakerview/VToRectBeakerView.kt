5944d2c7c000f1b85d72d20364d7f02ee4e6dab7
Anwesh43 <anweshthecool0@gmail.com>
2020-12-08 01:32:21 +0530

    adding constants and libraries

--- VToRectBeakerView/src/main/java/com/example/vtorectbeakerview/VToRectBeakerView.kt
+++ VToRectBeakerView/src/main/java/com/example/vtorectbeakerview/VToRectBeakerView.kt
@@ -1,2 +1,27 @@
 package com.example.vtorectbeakerview
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#3F51B5",
+    "#4CAF50",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val sizeFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+

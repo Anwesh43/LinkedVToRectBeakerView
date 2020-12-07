0c18196f38824f3545aba4fcd3a22f23071dba1c
Anwesh43 <anweshthecool0@gmail.com>
2020-12-08 01:41:03 +0530

    adding canvas extension methods

--- VToRectBeakerView/src/main/java/com/example/vtorectbeakerview/VToRectBeakerView.kt
+++ VToRectBeakerView/src/main/java/com/example/vtorectbeakerview/VToRectBeakerView.kt
@@ -30,3 +30,32 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawVToRectBeaker(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val x : Float = size * 0.5f * sf1
+    val y : Float = -size * sf2
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(0f, 0f, x, 0f, paint)
+        drawLine(0f, 0f, 0f, y, paint)
+        restore()
+    }
+    drawRect(RectF(-size / 2, -size * sf3, size / 2, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawVTRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h)  / strokeFactor
+    drawVToRectBeaker(scale, w, h, paint)
+}
